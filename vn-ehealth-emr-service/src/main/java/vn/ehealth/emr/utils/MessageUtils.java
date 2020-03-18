2a527262c6a7e27037e67a3c472e725615f89dae
tungdt <duongthanhtungvn01@gmail.com>
2020-03-18 10:13:06 +0700

    Update

---
+++ vn-ehealth-emr-service/src/main/java/vn/ehealth/emr/utils/MessageUtils.java
@@ -0,0 +1,30 @@
+package vn.ehealth.emr.utils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class MessageUtils {
+
+    private static Properties messages = new Properties();
+    
+    private static Logger logger = LoggerFactory.getLogger(MessageUtils.class);
+            
+    static {
+        try {
+            var input = new ClassPathResource("code_systems.properties").getInputStream();
+            messages.load(new InputStreamReader(input, Charset.forName("UTF-8")));
+        } catch (IOException e) {
+            logger.error("Cannot read user.properties", e);
+        }
+    }
+    
+    public static String getText(String property) {
+        return messages.getProperty(property, property);
+    }
+}
