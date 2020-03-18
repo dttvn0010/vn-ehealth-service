d1dfd70bd28c4ebc59af20726c56a72ee99d229a
tungdt <duongthanhtungvn01@gmail.com>
2020-03-18 10:20:29 +0700

    Update dev

--- vn-ehealth-hl7-fhir/src/main/java/vn/ehealth/hl7/fhir/providers/IHFResourceProvider.java
+++
@@ -1,8 +0,0 @@
-package vn.ehealth.hl7.fhir.providers;
-
-import ca.uhn.fhir.rest.server.IResourceProvider;
-
-public interface IHFResourceProvider extends IResourceProvider {
-
-    Long count();
-}
