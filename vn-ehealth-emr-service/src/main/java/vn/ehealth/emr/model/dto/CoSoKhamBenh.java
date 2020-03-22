196ac8180194ba0b28222a1726df647f598474d4
tungdt <duongthanhtungvn01@gmail.com>
2020-03-22 12:57:56 +0700

    Update Cskb <-> Organization

--- vn-ehealth-emr-service/src/main/java/vn/ehealth/emr/model/dto/CoSoKhamBenh.java
+++ vn-ehealth-emr-service/src/main/java/vn/ehealth/emr/model/dto/CoSoKhamBenh.java
@@ -6,7 +6,7 @@ import vn.ehealth.hl7.fhir.dao.util.DaoFactory;
 import static vn.ehealth.hl7.fhir.core.util.DataConvertUtil.*;
 import static vn.ehealth.hl7.fhir.core.util.FhirUtil.*;
 
-import org.hl7.fhir.r4.model.Location;
+import org.hl7.fhir.r4.model.Organization;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.ResourceType;
 
@@ -19,29 +19,29 @@ public class CoSoKhamBenh extends BaseModelDTO {
         super();
     }
     
-    public CoSoKhamBenh(Location obj) {
+    public CoSoKhamBenh(Organization obj) {
         super(obj);
         if(obj == null) return;        
         this.ma = obj.hasIdentifier()? obj.getIdentifierFirstRep().getValue() : "";
         this.ten = obj.hasName() ? obj.getName() : "";
     }
     
-    public static CoSoKhamBenh fromFhir(Location ent) {
-        if(ent == null) return null;
-        return new CoSoKhamBenh(ent);        
+    public static CoSoKhamBenh fromFhir(Organization obj) {
+        if(obj == null) return null;
+        return new CoSoKhamBenh(obj);        
     }
     
     public static CoSoKhamBenh fromReference(Reference ref) {
         if(ref != null && ref.hasReference()) {
-            var ent = DaoFactory.getLocationDao().read(createIdType(ref));
-            return fromFhir(ent);
+            var obj = DaoFactory.getOrganizationDao().read(createIdType(ref));
+            return fromFhir(obj);
         }
         return null;        
     }
     
-    public static Location toFhir(CoSoKhamBenh dto) {
+    public static Organization toFhir(CoSoKhamBenh dto) {
         if(dto == null) return null;
-        var obj = new Location();
+        var obj = new Organization();
         obj.setId(dto.id);
         obj.setIdentifier(listOf(createIdentifier(dto.ma, CodeSystemValue.CO_SO_KHAM_BENH)));
         obj.setName(dto.ten);
@@ -50,6 +50,6 @@ public class CoSoKhamBenh extends BaseModelDTO {
 
     @Override
     public ResourceType getType() {
-        return ResourceType.Location;
+        return ResourceType.Organization;
     }
 }
