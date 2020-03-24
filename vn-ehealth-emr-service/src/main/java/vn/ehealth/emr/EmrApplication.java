890fa7aac84d0d0923d322fd0cb4ac42cd8e72cb
tungdt <duongthanhtungvn01@gmail.com>
2020-03-24 10:53:06 +0700

    Restore EmrApplication

--- vn-ehealth-emr-service/src/main/java/vn/ehealth/emr/EmrApplication.java
+++ vn-ehealth-emr-service/src/main/java/vn/ehealth/emr/EmrApplication.java
@@ -1,18 +1,37 @@
 package vn.ehealth.emr;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import vn.ehealth.hl7.fhir.Hl7FhirRestfulServer;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+
 @SpringBootApplication
-@EnableMongoRepositories("vn.ehealth.emr.repository")
-@EntityScan("vn.ehealth.emr.model")
-@ComponentScan("vn.ehealth.emr")
+@EnableMongoRepositories("vn.ehealth")
+@EntityScan("vn.ehealth")
+@ComponentScan("vn.ehealth")
 public class EmrApplication {
 
+    @Autowired
+    ApplicationContext context;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(EmrApplication.class, args);
 	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new Hl7FhirRestfulServer(context),
+                "/R4/*");
+        registration.setName("FhirServlet");
+        return registration;
+    }
 }
