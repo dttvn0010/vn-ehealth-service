7df956558b557c760fa1a3534836f787fef25b6a
Tung Duong <duongthanhtungvn01@gmail.com>
2020-07-01 09:03:23 +0100

    Update

--- vn-ehealth-cdr-service/src/main/java/vn/ehealth/cdr/model/component/CanboYteDTO.java
+++ vn-ehealth-cdr-service/src/main/java/vn/ehealth/cdr/model/component/CanboYteDTO.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import vn.ehealth.auth.model.User;
-import vn.ehealth.cdr.model.CanboYte;
-import vn.ehealth.cdr.service.ServiceFactory;
 
 @JsonInclude(Include.NON_NULL)
 public class CanboYteDTO {
@@ -28,23 +26,7 @@ public class CanboYteDTO {
     public CanboYteDTO(String ten) {
         this.ten = ten;
     }
-    
-    public static EmrRef toEmrRef(CanboYteDTO dto) {
-        if(dto == null) return null;
-                
-        var ref = new EmrRef();
-        ref.identifier = dto.chungChiHanhNghe;
-        ref.name = dto.ten;
-        ref.className = CanboYte.class.getName();
         
-        var obj = ServiceFactory.getCanboYteService().getByChungChiHanhNghe(dto.chungChiHanhNghe).orElse(null);
-        if(obj != null) {
-            ref.objectId = obj.id;
-        }
-        
-        return ref;
-    }
-    
     public static Reference toRef(CanboYteDTO dto) {
         if(dto != null) {
             var ref = new Reference();
@@ -52,14 +34,5 @@ public class CanboYteDTO {
             return ref;
         }
         return null;
-    }    
-    
-    public static CanboYteDTO fromCanboYte(CanboYte obj) {
-        if(obj == null) return null;
-        
-        var dto = new CanboYteDTO();
-        dto.ten = obj.ten;
-        dto.chungChiHanhNghe = obj.chungChiHanhNghe;
-        return dto;
     }
 }
