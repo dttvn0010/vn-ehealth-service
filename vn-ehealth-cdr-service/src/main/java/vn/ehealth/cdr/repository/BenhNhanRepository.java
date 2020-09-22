1ea51dba3cddfc389d4ce02a4509edff86e00b1e
Tung Duong <duongthanhtungvn01@gmail.com>
2020-09-22 09:45:17 +0100

    Update

--- vn-ehealth-cdr-service/src/main/java/vn/ehealth/cdr/repository/BenhNhanRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.ehealth.cdr.repository;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import vn.ehealth.cdr.model.BenhNhan;
-
-public interface BenhNhanRepository extends MongoRepository<BenhNhan, ObjectId> {
-}
--- vn-ehealth-cdr-service/src/main/java/vn/ehealth/cdr/repository/HoSoBenhAnRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.ehealth.cdr.repository;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import vn.ehealth.cdr.model.HoSoBenhAn;
-
-public interface HoSoBenhAnRepository extends MongoRepository<HoSoBenhAn, ObjectId> {
-}
